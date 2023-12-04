9fb5884051acc310d8aa3441425ed8580a090e5b
Kaneko773 <harukakaneko773@gmail.com>
2023-12-04 16:59:19 +0900

    プロジェクト ファイルを追加します。

---
+++ Dealer.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Person.h"
+class Shoe;
+
+class Dealer : public Person
+{
+public:
+	Dealer();
+	Dealer(const char* name);
+	virtual ~Dealer();
+
+public:
+	bool play(Shoe* pShoe);	// ƒ^[ƒ“ˆ—
+};
