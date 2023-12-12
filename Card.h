bfbd20a12bf322fc6e7973c8c746592964de2a01
Kaneko773 <harukakaneko773@gmail.com>
2023-12-12 10:14:36 +0900

    set関数にエラーチェックを作成

--- Card.h
+++ Card.h
@@ -6,14 +6,31 @@ class Card
 public:
 	Card();
 	virtual ~Card();
+	
+	//今回は要らない
 	void operator=(const Card& other);//代入演算子
+	
 	bool operator==(const Card& other);//等値演算子
 
 	void set_mark(int mark) {
-		_mark = mark;
+		//エラーチェックをする
+		if (mark < 0 || 3 < mark) {
+			cout << "エラーが発生しました" << endl;
+			_mark = 0;
+		}
+		else {
+			_mark = mark;
+		}
 	}
 	void set_number(int number) {
-		_number = number;
+		//エラーチェックをする
+		if (number < 0 || 12 < number) {
+			cout << "エラーが発生しました" << endl;
+			_number = 0;
+		}
+		else {
+			_number = number;
+		}
 	}
 	int get_mark() const{
 		return _mark;
